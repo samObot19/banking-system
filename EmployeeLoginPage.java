4dc7588141886a2b4a644ce12d40a7c29d755599
samuel <samuelfikadesilassie@gmail.com>
2023-09-26 12:11:05 -0700

    commited

---
+++ EmployeeLoginPage.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class EmployeeLoginPage extends JFrame implements ActionListener{
+    
+    
+    public EmployeeLoginPage(){
+        
+    }
+    
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public static void main(String [] args){
+
+    }
+}
---
+++ EmployeeRegisterPage.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class EmployeeRegisterPage extends JFrame implements ActionListener{
+    public EmployeeRegisterPage(){
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
